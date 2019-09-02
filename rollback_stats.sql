d74774894367faa088485e9756901a9b97905b6a
Baki Sahin <bakisahin@yahoo.com>
2019-09-02 11:32:25 +0300

    Update rollback_stats.sql

--- rollback_stats.sql
+++ rollback_stats.sql
@@ -40,3 +40,11 @@ where s.taddr = t.addr
 and s.inst_id = t.inst_id
 and s.STATUS = 'KILLED'
 order by t.inst_id;
+
+---http://o-dba.blogspot.com/2017/01/how-to-view-current-transaction-status.html
+              
+select ss.sid, ss.serial#, ss.username, st.used_ublk, st.used_urec, ss.status, decode(st.flag,7683,'ONGOING',7811,'ROLLBACK', st.flag) tr_status,  sqt.command_name, ss.sql_id, ss.prev_sql_id
+from v$session ss , v$transaction st, V$sqlcommand sqt
+where ss.saddr = st.ses_addr
+and sqt.command_type = ss.command
+order by 3;
