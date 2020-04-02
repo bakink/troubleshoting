462970c66cac43fc9f4dbfb8f45f7622854ec490
Baki Sahin <bakisahin@yahoo.com>
2020-04-02 11:27:43 +0300

    Update PGA_and_Temp.sql

--- PGA_and_Temp.sql
+++ PGA_and_Temp.sql
@@ -25,10 +25,11 @@ and session_serial# = &serial
 order by 3;
 
 
-
+==============================================================
 
 Temp and PGA - Usage History 
 Source - https://bdrouvot.wordpress.com/2013/03/19/link-huge-pga-temp/
+https://bdrouvot.wordpress.com/2013/04/19/drill-down-to-sql_id-execution-details-in-ash/
 
 The output tells you which SQL_ID asked for maximum PGA w.r.t to PGA that was allocated for this particular session.
 
@@ -93,7 +94,53 @@ having sum(DELTA_TEMP_MB) > 0
 where rank < (&top+1)
 order by rank;
 
+==============================================================
+--https://bdrouvot.wordpress.com/2013/04/19/drill-down-to-sql_id-execution-details-in-ash/
+alter session set nls_date_format='YYYY/MM/DD HH24:MI:SS';
+alter session set nls_timestamp_format='YYYY/MM/DD HH24:MI:SS';
+ 
+select sql_id,
+      starting_time,
+      end_time,
+ (EXTRACT(HOUR FROM run_time) * 3600
+                    + EXTRACT(MINUTE FROM run_time) * 60
+                    + EXTRACT(SECOND FROM run_time)) run_time_sec,
+      READ_IO_BYTES,
+      PGA_ALLOCATED PGA_ALLOCATED_BYTES,
+      TEMP_ALLOCATED TEMP_ALLOCATED_BYTES
+from  (
+select
+       sql_id,
+       max(sample_time - sql_exec_start) run_time,
+       max(sample_time) end_time,
+       sql_exec_start starting_time,
+       sum(DELTA_READ_IO_BYTES) READ_IO_BYTES,
+       sum(DELTA_PGA) PGA_ALLOCATED,
+       sum(DELTA_TEMP) TEMP_ALLOCATED
+       from
+       (
+       select sql_id,
+       sample_time,
+       sql_exec_start,
+       DELTA_READ_IO_BYTES,
+       sql_exec_id,
+       greatest(PGA_ALLOCATED - first_value(PGA_ALLOCATED) over (partition by sql_id,sql_exec_id order by sample_time rows 1 preceding),0) DELTA_PGA,
+       greatest(TEMP_SPACE_ALLOCATED - first_value(TEMP_SPACE_ALLOCATED) over (partition by sql_id,sql_exec_id order by sample_time rows 1 preceding),0) DELTA_TEMP
+       from
+       dba_hist_active_sess_history
+       where
+       sample_time >= to_date ('2013/04/16 00:00:00','YYYY/MM/DD HH24:MI:SS')
+       and sample_time < to_date ('2013/04/16 03:10:00','YYYY/MM/DD HH24:MI:SS')
+       and sql_exec_start is not null
+       and IS_SQLID_CURRENT='Y'
+       )
+group by sql_id,SQL_EXEC_ID,sql_exec_start
+order by sql_id
+)
+where sql_id = 'btvk5dzpdmadh'
+order by sql_id, run_time_sec desc;
 
+=============================================================
 Sort Segment Usage Queries
 
 SELECT tablespace_name, TOTAL_BLOCKS, USED_BLOCKS,
